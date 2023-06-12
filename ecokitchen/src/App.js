0217224a3109a2483fe9ec47d2a6a82b61c32d4e
Theojeddy <Theojeddy@hotmail.co.uk>
2023-06-12 16:56:32 +0100

    buttons added to landing page

--- ecokitchen/src/App.js
+++ ecokitchen/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
+        <Route path="/LandingPage" element={<LandingPage />} />
       </Routes>
     </div>
   );
---
+++ ecokitchen/src/pages/LandingPage.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React from 'react'; 
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+
+
+export default function LandingPage() {
+    <Link to={`/LaningPage}`}></Link>;
+return(
+
+<div className="card flex flex-wrap justify-content-center gap-3">
+    <Button label="Login" rounded />
+<br/>
+
+    <Button label="Signup" rounded />
+<br/>
+    <Button label="Forgot Password" rounded />
+</div>
+
+)};
