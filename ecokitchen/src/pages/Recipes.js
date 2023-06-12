492effc5d764628b0b1027ad79dd7eedab98ea8b
saynab <saynabd4@gmail.com>
2023-06-12 14:49:54 +0100

    recipes & homepage

---
+++ ecokitchen/src/pages/Recipes.js
@@ -0,0 +1,8 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function Recipes() {
+  <Link to={`/recipes}`}></Link>;
+  return <div> This is the recipes page </div>;
+}
+
+export default Recipes;
