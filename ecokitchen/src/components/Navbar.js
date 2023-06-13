03a4a2a9d14611059ddb5bce178ae5f869acda96
Theojeddy <Theojeddy@hotmail.co.uk>
2023-06-13 12:34:39 +0100

    navbar recipie button functional, other buttons need completing

--- ecokitchen/src/components/Navbar.js
+++ ecokitchen/src/components/Navbar.js
@@ -3,6 +3,7 @@ import { BiFridge } from "react-icons/bi";
 import { BiListCheck } from "react-icons/bi";
 import { BiBarChartAlt } from "react-icons/bi";
 import { BiBookOpen } from "react-icons/bi";
+import {Link} from "react-router-dom"
 
 import '../App.css'
 
@@ -13,18 +14,31 @@ export default function Navbar() {
 
 return (
     <div className='navbar-container'>
-    <span classname='navbar-icons'>
+    
+    {/* Home icon */}
+        <span classname='navbar-icons'>
         <BiHomeAlt className='navbar-icons'/>
         Home</span>
+
+     {/* Fridge icon */}
         <span classname='navbar-icons'>
         <BiFridge className='navbar-icons'/>
         Food</span>
+    {/* Recipies icon */}
+
+        <Link to={`/recipes`}>
+        <div>
         <span classname='navbar-icons'>
         <BiBookOpen className='navbar-icons'/>
-        Recipies</span>
+        Recipies</span></div>
+        </Link>
+
+    {/* Shopping icon */}
         <span classname='navbar-icons'>
         <BiListCheck className='navbar-icons'/>
         Shopping</span>
+
+    {/* Breakdown icon */}
         <span classname='navbar-icons'>
         <BiBarChartAlt className='navbar-icons'/>
         Breakdown</span>
