7c24070f0b59db3079bd696c0e80bcbdec547988
Theojeddy <Theojeddy@hotmail.co.uk>
2023-06-13 10:33:26 +0100

    navbar basics added

--- ecokitchen/src/components/Navbar.js
+++ ecokitchen/src/components/Navbar.js
@@ -1,1 +1,45 @@
-//hi
+import { useState } from 'react';
+import { Dock } from 'primereact/dock';
+import { RadioButton } from 'primereact/radiobutton';
+import { IconName } from "react-icons/bi";
+
+
+export default function Navbar() {
+        
+    const items = [
+        {
+            label: 'Finder',
+            icon: () => <img alt="Finder" src="https://primefaces.org/cdn/primereact/images/dock/finder.svg" width="10%" />,
+        },
+        {
+            label: 'App Store',
+            icon: () => <img alt="App Store" src="https://primefaces.org/cdn/primereact/images/dock/appstore.svg" width="10%" />,
+        },
+        {
+            label: 'Photos',
+            icon: () => <img alt="Photos" src="https://primefaces.org/cdn/primereact/images/dock/photos.svg" width="10%" />,
+        },
+        {
+            label: 'BiHomeAlt',
+            icon: () => <img alt="BiHomeAlt" src="https://react-icons.github.io/react-icons/icons?name=bi" width="10%" />,
+        }
+    ];
+
+   
+
+    return (
+        <div className="card dock-demo">
+            <div className="flex flex-wrap gap-3 mb-5">
+    
+                        <div className="flex align-items-center">
+                            <RadioButton/>
+                           
+                        </div>
+                
+            </div>
+            <div className="dock-window" style={{ backgroundImage: 'url(https://primefaces.org/cdn/primereact/images/dock/window.jpg)' }}>
+                <Dock model={items}  />
+            </div>
+        </div>
+    )
+}
--- ecokitchen/src/pages/LandingPage.js
+++ ecokitchen/src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Button } from 'primereact/button';
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import styles from '../App.css'
+import Navbar from "../components/Navbar";
 
 export default function LandingPage() {
     <Link to={`/LaningPage}`}></Link>;
@@ -16,6 +17,10 @@ return(
     <Button label="Signup" rounded />
 <br/>
     <Button label="Forgot Password" rounded />
+<br/>
+    <Navbar/>
+
+
 
 </div>
 
