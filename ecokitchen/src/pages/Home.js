2cbca96d087387796fc01436edb7b61eddff2dd1
Theojeddy <Theojeddy@hotmail.co.uk>
2023-06-13 12:22:06 +0100

    nav bar aligned pending routing

--- ecokitchen/src/pages/Home.js
+++ ecokitchen/src/pages/Home.js
@@ -4,10 +4,26 @@ import '../App.css'
 
 function Home() {
   <Link to={`/HomePage}`}></Link>;
-  return <div> This is the homepage 
-  <br/>
-    <Navbar/></div>;
-}
+  return (
+    //Main page styling start
+    <div className="styles-container">
+      <div className="header-container"></div>
+      <div className="content-container">
+        <h1 className="page-title">Your Homepage</h1>
+        
+        <button className="donate-btn">Donate</button>
+        <input type="text" placeholder="Search" />
+        
+      </div>
+{/* main page styling end */}
+      <br/>
+      <Navbar/>
+      </div>
+      
+
+      
+  )
+};
 
 
 export default Home;
