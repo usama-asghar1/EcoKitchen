bc8b911678f4d454a4a8506e9311794b69f7f2c1
Carrie Shen <carrieshen.uk@gmail.com>
2023-06-13 12:18:30 +0100

    header prototype

---
+++ ecokitchen/src/pages/Donate.js
@@ -0,0 +1,8 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function Donate() {
+  <Link to={`/donate}`}></Link>;
+  return <h1> This is Donation Page.</h1>;
+}
+
+export default Donate;
