e4e1b6c3dd7390a120e90bfce8a7a93f787f7a84
ahiajalil <ahiajalil@gmail.com>
2023-06-14 16:33:41 +0100

    recipe page completed with api link

--- ecokitchen/src/pages/Recipes.js
+++ ecokitchen/src/pages/Recipes.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-function Recipes() {
+import "./Recipe.css";
 
+function Recipes() {
   const [ingredient, setIngredient] = useState("");
   const [recipeData, setRecipe] = useState([]);
   const [error, setError] = useState(null);
 
-
   async function fetchRecipe() {
-    setError(null);
+    setError(error);
 
     const response = await fetch(
       `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
@@ -23,26 +23,31 @@ function Recipes() {
     setRecipe(limitedRecipes);
   }
 
-
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Please input your main ingredient"
-        onChange={(e) => setIngredient(e.target.value)}
-      />
-       <button onClick={fetchRecipe}>Search</button>
+    <div className="recipe-container">
+      <div className="recipe-search-container">
+        <input
+          type="text"
+          placeholder="Please input your main ingredient"
+          onChange={(e) => setIngredient(e.target.value)}
+        />
+        <button onClick={fetchRecipe}>Search</button>
+      </div>
+
       {recipeData.length > 0 && (
-        <div>
+        <div className="recipes-grid">
           {recipeData.map((recipe) => (
             <div key={recipe.idMeal}>
-              <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+              <img
+                className="recipe-img"
+                src={recipe.strMealThumb}
+                alt={recipe.strMeal}
+              />
               <p>{recipe.strMeal}</p>
             </div>
           ))}
         </div>
       )}
-     
     </div>
   );
 }
