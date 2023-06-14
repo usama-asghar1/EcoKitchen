3259be00a17db8fdf3ad764cc6921eff1e86c408
Carrie Shen <carrieshen.uk@gmail.com>
2023-06-14 14:16:24 +0100

    set up API for recipe search

--- ecokitchen/src/pages/Recipes.js
+++ ecokitchen/src/pages/Recipes.js
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Recipes() {
- 
-  return <div> This is the recipes page </div>;
+
+  const [ingredient, setIngredient] = useState("");
+  const [recipeData, setRecipe] = useState([]);
+  const [error, setError] = useState(null);
+
+
+  async function fetchRecipe() {
+    setError(null);
+
+    const response = await fetch(
+      `www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    );
+    console.log(response);
+
+    const data = await response.json();
+    console.log(data);
+    setIngredient(data);
+  }
+
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Please input your address"
+        onChange={(e) => setRecipe(e.target.value)}
+      />
+      {recipeData.length > 0 && <p>{recipeData[0].strMeal}</p>}
+      <button onClick={fetchRecipe}>Fetch</button>
+    </div>
+  );
 }
 
 export default Recipes;
