c7b90ea0189e27a55be6092ec025a86f85906695
Theojeddy <Theojeddy@hotmail.co.uk>
2023-06-14 16:44:36 +0100

    button styling incomplete

--- ecokitchen/src/pages/Login-pages/Login.js
+++ ecokitchen/src/pages/Login-pages/Login.js
@@ -1,14 +1,23 @@
 import logo1 from "./LOGO.svg";
 import './Loginpages.css'
+import { Button } from 'primereact/button';
+import './theme.css'
 
 export default function Login() {
  
     return (
-    <div> This is the Login page
-    <div>
+      <div>
       <div className='logo-position-login-pages'>
       <img src={logo1} alt="Logo" className="logo-image-login-pages"></img>
       </div>
-    </div>
-    </div>
+  <br/>
+      <div className='button-position-login-pages'>
+      <Button label="Login" severity="success" rounded />
+  
+  </div>
+  
+  
+  
+  </div>
+  
   )};
