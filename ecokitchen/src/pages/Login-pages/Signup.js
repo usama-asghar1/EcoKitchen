a718fef8a492e043915325789ae64076953ce8d3
Theojeddy <Theojeddy@hotmail.co.uk>
2023-06-15 16:23:50 +0100

    login page and signup page functional but not styles

--- ecokitchen/src/pages/Login-pages/Signup.js
+++ ecokitchen/src/pages/Login-pages/Signup.js
@@ -1,41 +1,72 @@
 import logo1 from "./LOGO.svg";
-import './Loginpages.css'
+import './Loginpages.css';
 import React, { useState } from "react";
 import { Password } from 'primereact/password';
 import { InputText } from "primereact/inputtext";
 import { Button } from 'primereact/button';
-import {Link} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const [value, setValue] = useState('');
-  const [confirmPassword, setConfimPassword] = useState('');
-    return (
-      <div>
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const loginSubmit = {
+      Username: username,
+      Email: email,
+      Password: password,
+      ConfirmedPassword: confirmPassword
+    };
+
+    console.log(loginSubmit);
+
+    navigate('/Login');
+  };
+
+  return (
+    <div>
       <div className='logo-position-login-pages'>
-      <img src={logo1} alt="Logo" className="logo-image-login-pages"></img>
+        <img src={logo1} alt="Logo" className="logo-image-login-pages" />
       </div>
       <div className="flex flex-column gap-2">
-                <label htmlFor="username">Username</label>
-                <InputText id="username" aria-describedby="username-help" />
-        </div>
-        <div className="flex flex-column gap-2">
-                <label htmlFor="Email">Email</label>
-                <InputText id="Email" />
-        </div>
+        <label htmlFor="username">Username</label>
+        <InputText value={username} onChange={handleUsernameChange} id="username" aria-describedby="username-help" />
+      </div>
+      <div className="flex flex-column gap-2">
+        <label htmlFor="Email">Email</label>
+        <InputText value={email} onChange={handleEmailChange} id="Email" />
+      </div>
       <div className="card flex justify-content-center">
-      <label htmlFor="Password">Password</label>
-            <Password value={value} onChange={(e) => setValue(e.target.value)} toggleMask />
-        </div>
-        <div className="card flex justify-content-center">
-        <label htmlFor="Confirm Password"> Confim Password</label>
-            <Password confirmPassword={confirmPassword} onChange={(e) => setConfimPassword(e.target.confirmPassword)} toggleMask />
-        </div>
-        <Link className="link" to="/Login">
+        <label htmlFor="Password">Password</label>
+        <Password value={password} onChange={handlePasswordChange} toggleMask />
+      </div>
+      <div className="card flex justify-content-center">
+        <label htmlFor="Confirm Password">Confirm Password</label>
+        <Password value={confirmPassword} onChange={handleConfirmPasswordChange} toggleMask />
+      </div>
+      <Link className="link" to="/Login">
         <div className='button-position-login-pages'>
-      <Button label="Sign Up" rounded />
-      </div>
+          <Button onClick={handleSubmit} label="Sign Up" rounded />
+        </div>
       </Link>
-
-      </div>
-    
-  )};
+    </div>
+  );
+}
