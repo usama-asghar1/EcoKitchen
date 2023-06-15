1b8e0566b4311d340a82c9c28e2597dc834a0313
Theojeddy <Theojeddy@hotmail.co.uk>
2023-06-15 11:29:53 +0100

    Sign up Page, just missing Css

--- ecokitchen/src/pages/Login-pages/Signup.js
+++ ecokitchen/src/pages/Login-pages/Signup.js
@@ -1,16 +1,38 @@
 import logo1 from "./LOGO.svg";
 import './Loginpages.css'
+import React, { useState } from "react";
+import { Password } from 'primereact/password';
+import { InputText } from "primereact/inputtext";
+import { Button } from 'primereact/button';
 
 export default function SignUp() {
- 
+  const [value, setValue] = useState('');
+  const [confirmPassword, setConfimPassword] = useState('');
     return (
-    <div> This is the Sign up page
-    <div>
+      <div>
       <div className='logo-position-login-pages'>
       <img src={logo1} alt="Logo" className="logo-image-login-pages"></img>
       </div>
-    </div>
-    </div>
+      <div className="flex flex-column gap-2">
+                <label htmlFor="username">Username</label>
+                <InputText id="username" aria-describedby="username-help" />
+        </div>
+        <div className="flex flex-column gap-2">
+                <label htmlFor="Email">Email</label>
+                <InputText id="Email" />
+        </div>
+      <div className="card flex justify-content-center">
+      <label htmlFor="Password">Password</label>
+            <Password value={value} onChange={(e) => setValue(e.target.value)} toggleMask />
+        </div>
+        <div className="card flex justify-content-center">
+        <label htmlFor="Confirm Password"> Confim Password</label>
+            <Password confirmPassword={confirmPassword} onChange={(e) => setConfimPassword(e.target.confirmPassword)} toggleMask />
+        </div>
+        <div className='button-position-login-pages'>
+      <Button label="Sign Up" rounded />
+      </div>
+
+      </div>
+    
   )};
-  
- 
