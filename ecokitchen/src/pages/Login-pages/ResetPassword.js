61989d885871e4c24f9d2ec09ff6026b4861e6e2
saynab <saynabd4@gmail.com>
2023-06-15 13:56:40 +0100

    Reset password page - needs styling

--- ecokitchen/src/pages/Login-pages/ResetPassword.js
+++ ecokitchen/src/pages/Login-pages/ResetPassword.js
@@ -1,14 +1,42 @@
 import logo1 from "./LOGO.svg";
-import './Loginpages.css'
+import "./Loginpages.css";
+import { Password } from "primereact/password";
+import React, { useState } from "react";
+import { Button } from "primereact/button";
 
 export default function ResetPassword() {
- 
-    return (
-    <div> This is the Reset Password page
+  const [value, setValue] = useState("");
+  const [confirmPassword, setConfimPassword] = useState("");
+  return (
     <div>
-      <div className='logo-position-login-pages'>
-      <img src={logo1} alt="Logo" className="logo-image-login-pages"></img>
+      {" "}
+      <div>
+        <div className="logo-position-login-pages">
+          <img src={logo1} alt="Logo" className="logo-image-login-pages"></img>
+          <h2> RESET PASSWORD </h2>
+          <div className="card flex justify-content-center">
+            <p> New Password </p>
+            <div className="card flex justify-content-center">
+              <Password
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                toggleMask
+              />
+            </div>
+            <p> Confirm New Password </p>
+            <div className="card flex justify-content-center">
+              <Password
+                confirmPassword={confirmPassword}
+                onChange={(e) => setConfimPassword(e.target.confirmPassword)}
+                toggleMask
+              />
+            </div>
+            <div className="button-position-login-pages">
+              <Button label="Login" rounded />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-  )};
+  );
+}
