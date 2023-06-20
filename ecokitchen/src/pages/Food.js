0066c89e9cdcf02c3d969f84031f8fe576e1d832
Carrie Shen <carrieshen.uk@gmail.com>
2023-06-20 10:32:42 +0100

    added useState and useEffect for food page

--- ecokitchen/src/pages/Food.js
+++ ecokitchen/src/pages/Food.js
@@ -1,11 +1,49 @@
 //this is food
 
 import "../App.css";
+import { useState, useEffect } from "react";
+import supabase from "../components/supabase/supabaseClient";
 
 
 function Food() {
+
+const [fetchError, setFetchError] = useState(null)
+const [food, setFood] = useState(null)
+
+useEffect(() => {
+  const fetchFood = async () => {
+    const { data, error } = await supabase
+      .from('food_items')
+      .select()
+      
+      console.log(data);
+
+      if (error) {
+        setFetchError('Could not fetch food items')
+        setFood(null)
+        console.log(error)
+      }
+      if (data) {
+        setFood(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchFood();
+
+}, [])
   
-  return <div> This is the food page </div>;
+  return <div className="food-page"> 
+  <p>This is the Food Page</p>
+    {fetchError && (<p>{fetchError}</p>)}
+    {food && (
+      <div className="food">
+        {food.map((food) => (
+          <p>{food.name}</p>
+        ))}
+      </div>
+    )}
+  </div>;
 }
 
 export default Food;
