307b0d402ef4443b080969a74d111345d96bdfcc
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-06-22 09:53:32 +0100

    navbar style

--- ecokitchen/src/components/Navbar.js
+++ ecokitchen/src/components/Navbar.js
@@ -14,49 +14,62 @@ export default function Navbar() {
     const getIconColor = (path) => {
       return location.pathname === path ? 'navbar-icon-blue' : 'navbar-icon-grey';
     };
-  
+
     return (
-      <div className="navbar-container">
-        {/* Home icon */}
-        <Link className="link" to="/HomePage">
-          <div className={`navbar-icon ${getIconColor('/HomePage')}`}>
-            <BiHomeAlt className="navbar-icon-image" />
-            <span className="navbar-icon-name">Home</span>
+      <div className="navbar-container fixed-bottom">
+        <div className="container-fluid">
+          <div className="row">
+            {/* Home icon */}
+            <div className="col">
+              <Link className="link" to="/HomePage">
+                <div className={`navbar-icon ${getIconColor('/HomePage')}`}>
+                  <BiHomeAlt className="navbar-icon-image" />
+                  <span className="navbar-icon-name">Home</span>
+                </div>
+              </Link>
+            </div>
+    
+            {/* Fridge icon */}
+            <div className="col">
+              <Link className="link" to="/Food">
+                <div className={`navbar-icon ${getIconColor('/Food')}`}>
+                  <BiFridge className="navbar-icon-image" />
+                  <span className="navbar-icon-name">Food</span>
+                </div>
+              </Link>
+            </div>
+    
+            {/* Recipes icon */}
+            <div className="col">
+              <Link className="link" to="/Recipes">
+                <div className={`navbar-icon ${getIconColor('/Recipes')}`}>
+                  <BiBookOpen className="navbar-icon-image" />
+                  <span className="navbar-icon-name">Recipes</span>
+                </div>
+              </Link>
+            </div>
+    
+            {/* Shopping icon */}
+            <div className="col">
+              <Link className="link" to="/Shopping">
+                <div className={`navbar-icon ${getIconColor('/Shopping')}`}>
+                  <BiListCheck className="navbar-icon-image" />
+                  <span className="navbar-icon-name">Shopping</span>
+                </div>
+              </Link>
+            </div>
+    
+            {/* Breakdown icon */}
+            <div className="col">
+              <Link className="link" to="/Breakdown">
+                <div className={`navbar-icon ${getIconColor('/Breakdown')}`}>
+                  <BiBarChartAlt className="navbar-icon-image" />
+                  <span className="navbar-icon-name">Breakdown</span>
+                </div>
+              </Link>
+            </div>
           </div>
-        </Link>
-  
-        {/* Fridge icon */}
-        <Link className="link" to="/Food">
-          <div className={`navbar-icon ${getIconColor('/Food')}`}>
-            <BiFridge className="navbar-icon-image" />
-            <span className="navbar-icon-name">Food</span>
-          </div>
-        </Link>
-  
-        {/* Recipes icon */}
-        <Link className="link" to="/Recipes">
-          <div className={`navbar-icon ${getIconColor('/Recipes')}`}>
-            <BiBookOpen className="navbar-icon-image" />
-            <span className="navbar-icon-name">Recipes</span>
-          </div>
-        </Link>
-  
-        {/* Shopping icon */}
-        <Link className="link" to="/Shopping">
-          <div className={`navbar-icon ${getIconColor('/Shopping')}`}>
-            <BiListCheck className="navbar-icon-image" />
-            <span className="navbar-icon-name">Shopping</span>
-          </div>
-        </Link>
-  
-        {/* Breakdown icon */}
-        <Link className="link" to="/Breakdown">
-          <div className={`navbar-icon ${getIconColor('/Breakdown')}`}>
-            <BiBarChartAlt className="navbar-icon-image" />
-            <span className="navbar-icon-name">Breakdown</span>
-          </div>
-        </Link>
+        </div>
       </div>
     );
-  }
-  
+    }    
