0472e26bb59f6d949588e56534063d72374f51a1
leep1000 <leepalmerformal@gmail.com>
2023-06-26 11:39:51 +0100

    ShoppingCard is now fully functional

--- ecokitchen/src/pages/Shopping.js
+++ ecokitchen/src/pages/Shopping.js
@@ -7,24 +7,60 @@ function Shopping() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await supabase.auth.getUser();
-      const user = response.data.user.id;
-
-      const dataSB = await supabase
-        .from("Shopping_List")
-        .select("*")
-        .eq("user_id", user);
-      //convert from json to array
-      // const object = await result.json();
-      // const object2 = Object.values(object);
-      //remove this console log before deployment
-      setData(dataSB.data);
-      console.log(dataSB.data);
-    }
-
     fetchData();
   }, []);
+
+  async function fetchData() {
+    const response = await supabase.auth.getUser();
+    const user = response.data.user.id;
+
+    const dataSB = await supabase
+      .from("Shopping_List")
+      .select("*")
+      .eq("user_id", user);
+    //convert from json to array
+    // const object = await result.json();
+    // const object2 = Object.values(object);
+    //remove this console log before deployment
+    setData(dataSB.data);
+    console.log(dataSB.data);
+  }
+
+  async function increaseQuantity(foodID) {
+    const item = data.find((item) => item.id === foodID);
+    item.quantity += 1;
+
+    await supabase
+      .from("Shopping_List")
+      .update({ quantity: item.quantity })
+      .eq("id", foodID);
+
+    fetchData();
+  }
+
+  async function decreaseQuantity(foodID) {
+    const item = data.find((item) => item.id === foodID);
+
+    // if quantity is 0, do nothing
+    // We want this to happen since the user can keep common items in their shopping list
+    if (item.quantity === 0) {
+      return;
+    }
+
+    item.quantity -= 1;
+
+    await supabase
+      .from("Shopping_List")
+      .update({ quantity: item.quantity })
+      .eq("id", foodID);
+
+    fetchData();
+  }
+
+  async function deleteFood(foodID) {
+    await supabase.from("Shopping_List").delete().eq("id", foodID);
+    fetchData();
+  }
 
   const foodShoppingCard = data.map((foodItem) => {
     return (
@@ -34,6 +70,9 @@ function Shopping() {
         name={foodItem.name}
         quantity={foodItem.quantity}
         image_url={foodItem.image_url}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        deleteFood={deleteFood}
       />
     );
   });
